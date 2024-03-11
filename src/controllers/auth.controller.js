1a932648c13f44ddd16a5838bcda5254a6dad2a3
YameroAFK <henriquemartins7t@gmail.com>
2024-03-10 23:50:43 -0300

    10/03/2024 - Part 2

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,17 +6,19 @@ import { loginService } from '../services/auth.service.js'
 const login = async (req, res) => {
     const { email, senha } = req.body;
 
+    /* Pesquisa no banco de dados para validar se a senha e o usuário estão corretos, caso algum deles esteja incorreto, é exibido a mensagem de 
+    erro: "Usuario ou senha incorreto", se os dois estiverem corretos, ira exibir a mensagem dentro dos parenteses do "res.send()". */
     try {
         const usuario = await loginService(email);
-
-        const senhaisValid = bcrypt.compareSync(senha, usuario.senha);
 
         if (!usuario) {
             return res.status(404).send({ Mensagem: "Usuario ou senha incorreto" })
         }
 
+        const senhaisValid = bcrypt.compareSync(senha, usuario.senha);
+
         if (!senhaisValid) {
-            return res.status(400).send({ Mensagem: "Usuario ou senha incorreto" })
+            return res.status(404).send({ Mensagem: "Usuario ou senha incorreto" })
         }
 
         res.send("penis");
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,5 +1,10 @@
 import Usuario from '../models/Usuario.js'
 
+/* 
+    Const utilizando para chamar as informações que o usuário possui dentro do banco de dados pelo campo "email". Neste caso, .select(+senha) 
+    está sendo usado para, assim que for feita a requisição ao banco de dados, a senha também irá retornar nos campos, mas criptografada.
+*/
+
 const loginService = (email) => Usuario.findOne({ email: email }).select("+senha");
 
 export { loginService };
